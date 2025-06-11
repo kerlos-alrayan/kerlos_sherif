b950e9eefb48c87334d186fc5866f78239023e98
kerlos-alrayan <kerlossheref@gmail.com>
2025-06-11 22:16:45 +0300

    done skills view and projects

--- lib/features/projects/presentation/views/widgets/custo_carousels_lider_body.dart
+++ lib/features/projects/presentation/views/widgets/custo_carousels_lider_body.dart
@@ -19,58 +19,56 @@ class CustomCarouselSliderBody<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        width: 400,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          border: Border.all(
-            color: const Color(0xffE4E3E3),
+    return Container(
+      width: 400,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(
+          color: const Color(0xffE4E3E3),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Flexible(
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(16),
+                topRight: Radius.circular(16),
+                bottomRight: Radius.circular(16),
+                bottomLeft: Radius.circular(16),
+              ),
+              child: CachedNetworkImage(
+                fit: BoxFit.cover,
+                imageUrl: getImage(data),
+                width: double.infinity,
+                height: 250,
+                placeholder: (context, url) =>
+                const Center(child: CircularProgressIndicator(strokeWidth: 2)),
+                errorWidget: (context, url, error) =>
+                const Icon(Icons.error, color: Colors.red),
+              ),
+            ),
           ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Flexible(
-              child: ClipRRect(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(16),
-                  topRight: Radius.circular(16),
-                  bottomRight: Radius.circular(16),
-                  bottomLeft: Radius.circular(16),
-                ),
-                child: CachedNetworkImage(
-                  fit: BoxFit.cover,
-                  imageUrl: getImage(data),
-                  width: double.infinity,
-                  height: 250,
-                  placeholder: (context, url) =>
-                  const Center(child: CircularProgressIndicator(strokeWidth: 2)),
-                  errorWidget: (context, url, error) =>
-                  const Icon(Icons.error, color: Colors.red),
-                ),
-              ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Text(
+              getTitle(data),
+              overflow: TextOverflow.ellipsis,
+              style: Styles.textStyle30,
             ),
-            const SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: Text(
-                getTitle(data),
-                overflow: TextOverflow.ellipsis,
-                style: Styles.textStyle30,
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Text(
+              getDescription(data),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: Styles.textStyle18.copyWith(color: const Color(0xff767474)),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Text(
-                getDescription(data),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: Styles.textStyle18.copyWith(color: const Color(0xff767474)),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/features/projects/presentation/views/widgets/projects_grid_view_builder_body.dart
+++ lib/features/projects/presentation/views/widgets/projects_grid_view_builder_body.dart
@@ -20,59 +20,58 @@ class ProjectsGridViewBuilderBody<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        width: 400,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          border: Border.all(
-            color: const Color(0xffE4E3E3),
+    return Container(
+      width: 400,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(
+          color: const Color(0xffE4E3E3),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 8,
+        children: [
+          Flexible(
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(16),
+                topRight: Radius.circular(16),
+                bottomRight: Radius.circular(16),
+                bottomLeft: Radius.circular(16),
+              ),
+              child: CachedNetworkImage(
+                fit: BoxFit.cover,
+                imageUrl: getImage(data),
+                width: double.infinity,
+                height: 250,
+                placeholder: (context, url) => const Center(
+                    child: CircularProgressIndicator(strokeWidth: 2)),
+                errorWidget: (context, url, error) =>
+                    const Icon(Icons.error, color: Colors.red),
+              ),
+            ),
           ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          spacing: 8,
-          children: [
-            Flexible(
-              child: ClipRRect(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(16),
-                  topRight: Radius.circular(16),
-                  bottomRight: Radius.circular(16),
-                  bottomLeft: Radius.circular(16),
-                ),
-                child: CachedNetworkImage(
-                  fit: BoxFit.cover,
-                  imageUrl: getImage(data),
-                  width: double.infinity,
-                  height: 250,
-                  placeholder: (context, url) =>
-                  const Center(child: CircularProgressIndicator(strokeWidth: 2)),
-                  errorWidget: (context, url, error) =>
-                  const Icon(Icons.error, color: Colors.red),
-                ),
-              ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Text(
+              getTitle(data),
+              overflow: TextOverflow.ellipsis,
+              style: Styles.textStyle35,
             ),
-            const SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40),
-              child: Text(
-                getTitle(data),
-                overflow: TextOverflow.ellipsis,
-                style: Styles.textStyle35,
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 8),
+            child: Text(
+              getDescription(data),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style:
+                  Styles.textStyle22.copyWith(color: const Color(0xff767474)),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 8),
-              child: Text(
-                getDescription(data),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: Styles.textStyle22.copyWith(color: const Color(0xff767474)),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
